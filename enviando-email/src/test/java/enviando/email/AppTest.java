ddace89f899234db3b573ea51043eec915f39cf9
jhordanben <benaraujo74@gmail.com>
2024-05-14 11:33:56 -0300

    ATUALIZEI, AGORA DEU CERTO

--- enviando-email/src/test/java/enviando/email/AppTest.java
+++ enviando-email/src/test/java/enviando/email/AppTest.java
@@ -32,7 +32,7 @@ public class AppTest {
 		stringBuilderTextoEmail.append("Para ter acesso clique no botão abaixo.<br/><br/>");
 		stringBuilderTextoEmail.append("<b>Login:</b> jhordan@gmail.com<br/>");
 		stringBuilderTextoEmail.append("<b>Senha:</b> jhordanzao<br/>");
-		stringBuilderTextoEmail.append("<a target=\"_blank\" href=\"https://www.instagram.com style=\"color:#2525a7; padding: 14px 25px; text-align:center; text-decoration: none; display:inline-block; border-radius:30px; font-size:20px; font-family:courier; border : 3px solid green;background-color:#99DA39;\">Acessar o Instagram</a>"");
+		stringBuilderTextoEmail.append("<a target=\"_blank\" href=\"https://www.instagram.com style=\"color:#2525a7; padding: 14px 25px; text-align:center; text-decoration: none; display:inline-block; border-radius:30px; font-size:20px; font-family:courier; border : 3px solid green;background-color:#99DA39;\">Acessar o Instagram</a>");
 		
 		stringBuilderTextoEmail.append("<span style=\"font-size:8px\">Ass.: jhordan</span>");
 		
